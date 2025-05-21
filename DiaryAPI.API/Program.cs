ea6af4ab7f7f6adc2e2a96c46eeff7d6ddaca7f5
yusufabonesepeti <yusuf@abonesepeti.com>
2025-05-21 17:16:06 +0300

    feat: enhance Swagger configuration with JWT authentication support

--- DiaryAPI.API/Program.cs
+++ DiaryAPI.API/Program.cs
@@ -1,12 +1,41 @@
 using DiaryAPI.API.Middlewares;
 using DiaryAPI.Business;
 using DiaryAPI.DataAccess;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Diary API", Version = "v1" });
+    
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\""
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 builder.Services.AddDataAccessServices();
 builder.Services.AddBusinessServices(builder.Configuration);
 
